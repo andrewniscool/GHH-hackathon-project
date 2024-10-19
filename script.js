19c671f36ba66062641e515d1e5afb674f5f8869
andrewniscool <andrewn980123@gmail.com>
2024-10-19 13:10:55 -0400

    isOpen function

--- script.js
+++ script.js
@@ -30,3 +30,57 @@ function convertToEasternTime(utcDate) {
 
 // Call the function to show the current UTC time and Eastern Time
 showCurrentTime();
+function isOpen(diningHall){
+  const currentDate = new Date();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  if(currentDate.getDay() === 0 || currentDate.getDay() === 6){
+    if(diningHall === "newcomb"){
+      if (currentDate.getDay() === 6){
+        return false;
+      }
+      else if(hour >= 10 && hour < 20){
+        return true;
+      }
+    }
+    if(diningHall === "runk"){
+      if(hour >= 10 && (hour < 21 && minute <= 30)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    if(diningHall === "ohill"){
+      if(hour >= 8 && hour < 20){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  if(currentDate.getDay() >= 1 && currentDate.getDay() <= 5){
+    if(currentDate.getDay() === 5 && diningHall === "newcomb"){
+      if(hour >= 7 && (hour < 14 && minute < 15)){
+        return true;
+      }
+      return false;
+    }
+    else if(diningHall === "runk"){
+      if(hour >= 7 && (hour < 21 && minute < 30)){
+        return true;
+      }
+      return false;
+    }
+    else{
+      if(hour > 7 && hour < 20){
+        return true;
+      }
+      return false;
+    }
+  }
+}
+console.log(isOpen("ohill"))
+console.log(isOpen("newcomb"))
+console.log(isOpen("runk"))
